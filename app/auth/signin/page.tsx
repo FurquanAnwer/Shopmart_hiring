54c21c3f4a4c3bc4e539410a8e0017ac3a605daa
Furquan Anwer <furquan.anwer@gmail.com>
2024-08-19 01:06:26 +0530

    type check done and formevent imported

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { supabase } from '../../../lib/supabase';
 import { useRouter } from 'next/navigation';
+import { FormEvent } from 'react';
 import './signIn.css';
 
 export default function SignIn() {
