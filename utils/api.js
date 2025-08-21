f5d475e7bda9474c0bde3b8d5a3ef5acc74645ba
Furquan Anwer <furquan.anwer@gmail.com>
2025-08-21 12:05:49 +0530

    removed one commented line

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,6 @@
 // src/utils/api.js
 export const fetchProducts = async () => {
     const response = await fetch('https://fakestoreapi.com/products');
-    // const response = await fetch('https://dummyjson.com/products');
     const products = await response.json();
     return products;
   };
