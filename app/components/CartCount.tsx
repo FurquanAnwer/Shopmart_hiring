4f09be75e5f6d4fe538f68f7a44edfa53bdd69c0
Furquan Anwer <furquan.anwer@gmail.com>
2025-08-21 20:20:55 +0530

    fixed cart flickering on refresh bug and changed the background colour of header and font as well. Added a small margin between search input box and search button

--- app/components/CartCount.tsx
+++ app/components/CartCount.tsx
@@ -1,60 +1,35 @@
-// src/components/CartCount.js
 "use client";
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { RootState } from '@/redux/store';
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
 
 const CartCount = () => {
   const router = useRouter();
   const items = useSelector((state: RootState) => state.cart.items);
-  var quantiyNumbers : number = 0;
-  for(let i=0;i<items.length;i++){
-      quantiyNumbers += items[i].quantity;
-  }
+
+  // calculate total quantity
+  const quantityNumbers = items.reduce((acc, item) => acc + item.quantity, 0);
 
   const navigateToCart = () => {
-    router.push('/cart'); // Adjust the path based on your actual Cart Page route
-  };
-
-  const containerStyle:React.CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-  const iconContainerStyle:React.CSSProperties = {
-    cursor: 'pointer',
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-  const iconStyle:React.CSSProperties = {
-    fontSize: '30px',
-    color: '#00050b',
-  };
-
-  const countStyle:React.CSSProperties = {
-    position: 'absolute',
-    top: '-10px',
-    right: '-10px',
-    backgroundColor: 'white',
-    color: 'black',
-    borderRadius: '100%',
-    padding: '2px 5px',
-    fontSize: '12px',
-    fontWeight:"bold",
+    router.push("/cart");
   };
 
   return (
-    <div style={containerStyle}>
-      <div style={iconContainerStyle} onClick={navigateToCart}>
-        <FontAwesomeIcon icon={faShoppingCart} style={iconStyle} />
-        {/* Optional: Add a count badge if needed */}
-        <span style={countStyle}>{quantiyNumbers}</span>
+    <div className="flex items-center">
+      <div
+        className="relative flex items-center cursor-pointer"
+        onClick={navigateToCart}
+      >
+        {/* Cart Icon */}
+        <FontAwesomeIcon icon={faShoppingCart} className="text-white text-3xl" />
+
+        {/* Count Badge */}
+        <span className="absolute -top-2 -right-2 bg-white text-black rounded-full px-2 text-xs font-bold">
+          {quantityNumbers}
+        </span>
       </div>
     </div>
   );
