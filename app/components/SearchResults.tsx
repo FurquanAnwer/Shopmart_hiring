ad799a9b5b7b8f5038bc0ccf8233c04cd1f95640
Furquan Anwer <furquan.anwer@gmail.com>
2025-08-23 21:51:27 +0530

    Fixed Search functionality

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { fetchProducts } from "../../utils/api";
 import ProductCard from "../components/ProductCard";
 import SkeletonGrid from "./SkeletonGrid";
+import { useRouter } from "next/navigation";
 
 interface Product {
   id: number;
@@ -24,6 +25,7 @@ export default function SearchResults() {
   const query = searchParams.get("q");
   const [products, setProducts] = useState<Product[]>([]);
   const [loading,setLoading] = useState(true)
+  const router = useRouter()
 
   useEffect(() => {
     const getProducts = async () => {
@@ -32,7 +34,7 @@ export default function SearchResults() {
         setProducts(data);
       }
       catch{
-        console.log(`Unable to fetched searched product with the query ${q}`)
+        console.log(`Unable to fetched searched product with the query ${query}`)
       }
       finally{
         setLoading(false)
@@ -41,12 +43,13 @@ export default function SearchResults() {
     getProducts()
   }, []);
 
-  
+  if(!query)router.push('/')
   const regex = new RegExp(`\\b${query}\\b`, "i");
   const filteredProducts = products.filter(
     (p) => regex.test(p.title) || regex.test(p.description)
-  );
+  )
   const noOfProducts = filteredProducts.length;
+
 
   return (
     <div className="container mx-auto px-4 m-2">
@@ -54,14 +57,14 @@ export default function SearchResults() {
           loading?
           <SkeletonGrid/>:
           <div>
-            <div className="flex flex-row justify-center text-gray-700 text-2xl mb-10 mt-3">
-              {noOfProducts} matching products found!
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {filteredProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
+              <div className="flex flex-row justify-center text-gray-700 text-2xl mb-10 mt-3">
+                {noOfProducts} matching products found!
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {filteredProducts.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))}
+              </div>
 
           </div>          
       }
